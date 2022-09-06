7e7df550a7a836011d59fae54d1e214c6bfe2f06
Seraphic <danilsocol-2002@mail.ru>
2022-09-06 19:11:18 +0500

    Part 2 start

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,17 +1,25 @@
 package com.example.demoweb.model;
 
-public class Post {
-    String text;
-    Integer likes;
+import java.util.Date;
 
-    public Post(String _text, Integer _likes){
+public class Post {
+    private String text;
+    private Integer likes;
+    private Date creationDate;
+
+    public Post(String _text,Date _creationDate, Integer _likes){
         text = _text;
         likes = _likes;
+        creationDate = _creationDate;
     }
     public String getText(){
         return text;
     }
     public Integer getLikes(){
         return likes;
+    }
+
+    public Date getCreationDate(){
+        return creationDate;
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,6 +4,8 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,9 +13,9 @@ public class PostService {
 
     public ArrayList<Post> listAllPosts(){
         ArrayList<Post> list = new ArrayList<Post>();
-        list.add(new Post("Мужик",10));
-        list.add(new Post("Девушка",1000));
-        list.add(new Post("Мальчик",0));
+        list.add(new Post("Мужик",new Date(1978, 4,1,10,32),10));
+        list.add(new Post("Девушка",new Date(1978, 7,1,12,59),1000));
+        list.add(new Post("Мальчик",new Date(1978, 9,1,3,11),0));
         return list;
     }
 }
